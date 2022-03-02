091141c289258ec3fce61f947037f951a8b0b487
Yan Tuzlukov <tyzluckoff@yandex.ru>
2022-03-03 00:05:09 +0700

    third commit

--- src/test/java/ru/drom/utils/FirstUtil.java
+++ src/test/java/ru/drom/utils/FirstUtil.java
@@ -1,4 +1,8 @@
 package ru.drom.utils;
 
 public class FirstUtil {
+
+    public String utilMethod(final String firstVar, final String secondVar) {
+        return (firstVar + " " + secondVar);
+    }
 }
