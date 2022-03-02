92c0a8dc0bd8f8896b74ffc622f25033f6771859
Yan Tuzlukov <tyzluckoff@yandex.ru>
2022-03-03 00:01:37 +0700

    first commit

--- src/test/java/ru/drom/tests/TestBase.java
+++ src/test/java/ru/drom/tests/TestBase.java
@@ -2,7 +2,6 @@ package ru.drom.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
